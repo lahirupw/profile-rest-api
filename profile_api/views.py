1d76a295b148ef5dcd3fc8d50561bc282955e896
Prasad <lahirupw@gmail.com>
2020-11-11 06:23:02 +0530

    View and API URLs created

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API view"""
+
+    def get(self, request,format=None):
+        """Return a list of APIview Features"""
+        an_apiview =[
+            'Uses HTTP method as function (get,post,patch, delete)',
+            'Is similar to a traditional django view',
+            'Gives you the most control over the application logic',
+            'Mapped Manually to URLs',
+        ]
+        return Response({'message':'Hello','an_apiview': an_apiview})
