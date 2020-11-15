73566650d43e66efc7e7e824040ae1c8193fbf2d
Prasad <lahirupw@gmail.com>
2020-11-15 21:27:50 +0530

    Feed Model, Serializer and View created

--- profile_api/admin.py
+++ profile_api/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from profile_api import models
 
 admin.site.register(models.UserProfile)
+admin.site.register(models.ProfileFeedItem)
 # Register your models here.
--- profile_api/urls.py
+++ profile_api/urls.py
@@ -6,11 +6,13 @@ from profile_api import views
 router = DefaultRouter()
 router.register('hello-viewset',views.HelloViewSets,base_name='hello-viewset')
 router.register('profile',views.UserProfileViewSet)
+router.register('feed',views.UserProfileFeedViewset)
 
 
 urlpatterns = [
     path('hello-view/',views.HelloApiView.as_view()),
     path('login/', views.UserLoginApiView.as_view()),
     path('',include(router.urls))
+
 
 ]
